faf4e3a8b497638f7334879d78fdaf059e34f7a2
Ravi Bhaskar <robinkr1249@gmail.com>
2025-05-08 11:22:44 +0530

    index changed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 const corsOptions = {
-    origin:"http://localhost:3000",
+    origin:true,
     credentials:true,
 };
 
